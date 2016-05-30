4c7dd46fa3927f55d882e3e09ef129315d5debe8
Mahmood Shafeie Zargar <mahmood@gmail.com>
2016-05-30 15:38:42 -0400

    Resolving compatibility issue with El Capitan
    [Alex Harper's website](http://ragingmenace.com/) has not been
    updated since about a year ago and [his last post](
    http://ragingmenace.com/software/menumeters/index.html#requirement)
    seems to indicate that he does not have immediate plans to fix
    MenuMeter's compatibility issue with El Capitan. Meanwhile a user
    [has managed to have MM work on El Capitan](
    http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/) with
    minimal source code modifications. His version has become [the de facto
    standard distribution](http://apple.stackexchange.com/questions/198347)
    in the community. He has also released a couple of bug fixes ever since.
    Many website consider the original version as
    [discontinued](http://alternativeto.net/software/menumeters/).

--- Casks/menumeters.rb
+++ Casks/menumeters.rb
@@ -1,13 +1,13 @@
 cask 'menumeters' do
   version :latest
   sha256 :no_check
+  url 'http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/zips/MenuMeters_1.9.2.zip'
 
-  url 'http://www.ragingmenace.com/software/download/MenuMeters.dmg'
   name 'MenuMeters'
-  homepage 'http://www.ragingmenace.com/software/menumeters/'
+  homepage 'http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/'
   license :gpl
 
-  prefpane 'MenuMeters Installer.app/Contents/Resources/MenuMeters.prefPane'
+  prefpane 'MenuMeters.prefPane'
 
   zap delete: '~/Library/Preferences/com.ragingmenace.MenuMeters.plist'
 end
